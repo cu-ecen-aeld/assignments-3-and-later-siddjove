1662226fd15f1b42304dd9c49d7fb5c6763edbfe
Sidd Jove <siddjove@gmail.com>
2025-11-11 10:25:15 +0530

    Implemented systemcalls.c - all unit tests passing

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,99 +1,138 @@
 #include "systemcalls.h"
+#include <stdlib.h>     // for system(), malloc, free
+#include <stdio.h>      // for perror()
+#include <stdbool.h>    // for bool type
+#include <unistd.h>     // for fork(), execvp()
+#include <sys/wait.h>   // for waitpid()
+#include <fcntl.h>      // for open(), O_* flags
+#include <errno.h>      // for errno
 
 /**
  * @param cmd the command to execute with system()
- * @return true if the command in @param cmd was executed
- *   successfully using the system() call, false if an error occurred,
- *   either in invocation of the system() call, or if a non-zero return
- *   value was returned by the command issued in @param cmd.
-*/
+ * @return true if the command executed successfully, false otherwise
+ */
 bool do_system(const char *cmd)
 {
+    if (cmd == NULL)
+    {
+        return false;
+    }
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+    int ret = system(cmd);
+    if (ret == -1)
+    {
+        perror("system");
+        return false;
+    }
 
-    return true;
+    // WIFEXITED checks if child terminated normally
+    return WIFEXITED(ret) && (WEXITSTATUS(ret) == 0);
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
-
+ * @param count The number of arguments in the variable argument list
+ * @param ... Argument list where the first element is the program to execute
+ * @return true if the command executed successfully, false otherwise
+ */
 bool do_exec(int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
+
+    char *command[count + 1];
+    for (int i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
-
     va_end(args);
 
-    return true;
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        perror("fork");
+        return false;
+    }
+
+    if (pid == 0)
+    {
+        // Child process: replace image with execvp
+        execvp(command[0], command);
+        perror("execvp"); // only reached on error
+        exit(EXIT_FAILURE);
+    }
+
+    int status;
+    if (waitpid(pid, &status, 0) == -1)
+    {
+        perror("waitpid");
+        return false;
+    }
+
+    return WIFEXITED(status) && (WEXITSTATUS(status) == 0);
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
+ * @param outputfile Path to output file where stdout/stderr should be redirected
+ * @param count Number of arguments in varargs
+ * @param ... Argument list where first element is program to execute
+ * @return true if the command executed successfully, false otherwise
+ */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
+
+    char *command[count + 1];
+    for (int i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
-
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
-
     va_end(args);
 
-    return true;
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        perror("fork");
+        return false;
+    }
+
+    if (pid == 0)
+    {
+        // Open the output file for redirection
+        int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd < 0)
+        {
+            perror("open");
+            exit(EXIT_FAILURE);
+        }
+
+        // Redirect stdout and stderr
+        if (dup2(fd, STDOUT_FILENO) < 0 || dup2(fd, STDERR_FILENO) < 0)
+        {
+            perror("dup2");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+
+        close(fd);
+
+        // Execute command
+        execvp(command[0], command);
+        perror("execvp"); // only reached if exec fails
+        exit(EXIT_FAILURE);
+    }
+
+    int status;
+    if (waitpid(pid, &status, 0) == -1)
+    {
+        perror("waitpid");
+        return false;
+    }
+
+    return WIFEXITED(status) && (WEXITSTATUS(status) == 0);
 }
+
