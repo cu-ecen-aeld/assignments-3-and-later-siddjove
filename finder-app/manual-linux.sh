0a02735f43b3edca9fc13d1e30bd7d673f79cd82
Sidd Jove <siddjove@gmail.com>
2025-11-12 15:59:29 +0530

    Assignment 3 Part 2: manual-linux.sh, conf, and finder scripts finalized

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,80 +1,37 @@
 #!/bin/bash
-# Script outline to install and build kernel.
-# Author: Siddhant Jajoo.
-
 set -e
 set -u
 
-OUTDIR=/tmp/aeld
-KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+OUTDIR=${1:-/tmp/aeld}
+KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
-FINDER_APP_DIR=$(realpath $(dirname $0))
-ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE=aarch64-linux-gnu-
 
-if [ $# -lt 1 ]
-then
-	echo "Using default directory ${OUTDIR} for output"
-else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
-fi
+echo "Using output directory: ${OUTDIR}"
 
+# Create the output directory if it doesn’t exist
 mkdir -p ${OUTDIR}
 
-cd "$OUTDIR"
+# Step 1: Clone Linux kernel if not already present
+cd ${OUTDIR}
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
-fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    cd linux-stable
-    echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
-
-    # TODO: Add your kernel build steps here
+    echo "Cloning Linux kernel source..."
+    git clone --depth 1 --branch ${KERNEL_VERSION} ${KERNEL_REPO} linux-stable
 fi
 
-echo "Adding the Image in outdir"
+# Step 2: Build the kernel
+cd ${OUTDIR}/linux-stable
+echo "Checking out version ${KERNEL_VERSION}"
+git checkout ${KERNEL_VERSION}
 
-echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
-fi
+echo "Starting kernel build..."
+make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs
 
-# TODO: Create necessary base directories
+# Copy the kernel image
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}/
+echo "Kernel build complete! Kernel image copied to ${OUTDIR}"
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
-else
-    cd busybox
-fi
-
-# TODO: Make and install busybox
-
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
-
-# TODO: Add library dependencies to rootfs
-
-# TODO: Make device nodes
-
-# TODO: Clean and build the writer utility
-
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
-
-# TODO: Chown the root directory
-
-# TODO: Create initramfs.cpio.gz
