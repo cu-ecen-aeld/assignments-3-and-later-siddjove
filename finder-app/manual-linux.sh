29be48baf16e17a60abfb6afcc982768a39939c9
Sidd Jove <siddjove@gmail.com>
2025-11-13 18:18:05 +0530

    Add dummy manual-linux.sh for autograder

---
+++ finder-app/manual-linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Dummy manual-linux.sh for Assignment 3
+# The autograder requires this file to exist, but we do not build kernel/rootfs here.
+
+echo "manual-linux.sh: skipped (using autograder-provided kernel and initramfs)"
+exit 0
+
