469d3ddc5e57e6ae1c75b5f9a7187105e04662b5
Sidd Jove <siddjove@gmail.com>
2025-12-22 18:58:14 +0530

    Fix daemonization: foreground execution for init.d

--- server/aesdsocket.h
+++ server/aesdsocket.h
@@ -1,11 +1,18 @@
+#ifndef AESDSOCKET_H
+#define AESDSOCKET_H
 
-#define MAX_PACKET_SIZE 65535 
+#include <netinet/in.h>
 
+#define MAX_PACKET_SIZE 65536
+#define DATA_FILE "/var/tmp/aesdsocketdata"
 
-struct clientInfo{
+struct clientInfo {
     int clientFD;
-    struct sockaddr_in* clientAddr;
+    struct sockaddr_in clientAddr;
 };
 
-int server_init();
-void processClientData(struct clientInfo* clientInfo );
+int server_init(void);
+void *processClientData(void *arg);
+
+#endif
+
