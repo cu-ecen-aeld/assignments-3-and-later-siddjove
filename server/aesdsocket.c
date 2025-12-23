cb5e4f3ad48026f9a152f322ad5fcd2a1851d601
Sidd Jove <siddjove@gmail.com>
2025-12-23 13:20:47 +0530

    Fix aesdsocket implementation (thread-safe, correct file IO)

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,73 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 #include <signal.h>
 #include <fcntl.h>
-#include <pthread.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <syslog.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
 
-#include "aesdsocket.h"
+#define PORT 9000
+#define DATA_FILE "/var/tmp/aesdsocketdata"
+#define MAX_PACKET_SIZE 1024
 
 static volatile sig_atomic_t keepRunning = 1;
 
-static void sig_handler(int signo)
+struct clientInfo {
+    int clientFD;
+    struct sockaddr_in clientAddr;
+};
+
+void signal_handler(int signo)
 {
     (void)signo;
     keepRunning = 0;
-}
-
-int server_init(void)
-{
-    int sockfd;
-    struct sockaddr_in addr = {0};
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        return -1;
-
-    int enable = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
-
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(9000);
-    addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-        return -1;
-
-    return sockfd;
 }
 
 void *processClientData(void *arg)
 {
     struct clientInfo *client = (struct clientInfo *)arg;
     char buffer[MAX_PACKET_SIZE];
+    ssize_t bytes;
 
     int fd = open(DATA_FILE, O_CREAT | O_RDWR | O_APPEND, 0644);
-    if (fd < 0)
-        goto cleanup;
-
-    ssize_t bytes;
-    while ((bytes = recv(client->clientFD, buffer, sizeof(buffer), 0)) > 0) {
-
-        /* write received data */
-        write(fd, buffer, bytes);
-
-        /* rewind and send full file back */
-        lseek(fd, 0, SEEK_SET);
-        ssize_t r;
-        while ((r = read(fd, buffer, sizeof(buffer))) > 0) {
-            send(client->clientFD, buffer, r, 0);
-        }
-        lseek(fd, 0, SEEK_END);
+    if (fd < 0) {
+        syslog(LOG_ERR, "File open failed");
+        close(client->clientFD);
+        free(client);
+        return NULL;
     }
 
-cleanup:
+    while ((bytes = recv(client->clientFD, buffer, sizeof(buffer), 0)) > 0) {
+
+        write(fd, buffer, bytes);
+
+        if (memchr(buffer, '\n', bytes)) {
+            lseek(fd, 0, SEEK_SET);
+
+            char sendbuf[MAX_PACKET_SIZE];
+            ssize_t read_bytes;
+            while ((read_bytes = read(fd, sendbuf, sizeof(sendbuf))) > 0) {
+                send(client->clientFD, sendbuf, read_bytes, 0);
+            }
+        }
+    }
+
+    syslog(LOG_INFO, "Closed connection from %s",
+           inet_ntoa(client->clientAddr.sin_addr));
+
     close(fd);
     close(client->clientFD);
     free(client);
@@ -80,46 +72,71 @@ int main(int argc, char *argv[])
     int opt;
 
     while ((opt = getopt(argc, argv, "d")) != -1) {
-        if (opt == 'd')
-            daemonize = 1;
+        if (opt == 'd') daemonize = 1;
     }
 
-    openlog("aesdsocket", LOG_PID, LOG_USER);
+    openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
 
-    struct sigaction sa = {0};
-    sa.sa_handler = sig_handler;
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = signal_handler;
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
 
-    int sockfd = server_init();
-    if (sockfd < 0)
+    int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockFD < 0) {
+        syslog(LOG_ERR, "Socket creation failed");
         return -1;
+    }
+
+    int reuse = 1;
+    setsockopt(sockFD, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockFD, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+        syslog(LOG_ERR, "Bind failed");
+        close(sockFD);
+        return -1;
+    }
+
+    if (listen(sockFD, 10) < 0) {
+        syslog(LOG_ERR, "Listen failed");
+        close(sockFD);
+        return -1;
+    }
 
     if (daemonize) {
-        if (fork() > 0)
-            exit(0);
+        pid_t pid = fork();
+        if (pid < 0) exit(EXIT_FAILURE);
+        if (pid > 0) exit(EXIT_SUCCESS);
+
         setsid();
         chdir("/");
+        umask(0);
+
         close(STDIN_FILENO);
         close(STDOUT_FILENO);
         close(STDERR_FILENO);
-        open("/dev/null", O_RDWR);
-        dup(0);
-        dup(0);
     }
 
-    listen(sockfd, 5);
     remove(DATA_FILE);
 
     while (keepRunning) {
-        struct clientInfo *client = malloc(sizeof(*client));
-        socklen_t len = sizeof(client->clientAddr);
+        struct clientInfo *client = malloc(sizeof(struct clientInfo));
+        socklen_t addrlen = sizeof(client->clientAddr);
 
-        client->clientFD =
-            accept(sockfd, (struct sockaddr *)&client->clientAddr, &len);
+        client->clientFD = accept(sockFD,
+                                  (struct sockaddr *)&client->clientAddr,
+                                  &addrlen);
 
         if (client->clientFD < 0) {
             free(client);
+            if (errno == EINTR) break;
             continue;
         }
 
@@ -131,7 +148,7 @@ int main(int argc, char *argv[])
         pthread_detach(tid);
     }
 
-    close(sockfd);
+    close(sockFD);
     remove(DATA_FILE);
     closelog();
     return 0;
--- server/aesdsocket.h
+++ server/aesdsocket.h
@@ -1,18 +1,13 @@
-#ifndef AESDSOCKET_H
-#define AESDSOCKET_H
 
-#include <netinet/in.h>
+#define MAX_PACKET_SIZE 65535 
 
-#define MAX_PACKET_SIZE 65536
-#define DATA_FILE "/var/tmp/aesdsocketdata"
 
-struct clientInfo {
+struct clientInfo{
     int clientFD;
-    struct sockaddr_in clientAddr;
+    struct sockaddr_in* clientAddr;
 };
 
-int server_init(void);
-void *processClientData(void *arg);
+int server_init();
+void processClientData(struct clientInfo* clientInfo );
 
-#endif
 
